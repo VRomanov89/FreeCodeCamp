8c9f4e23e12a9912a2f0a9dedb8badd9b4b63034
Vladimir Romanov <vladromweb@gmail.com>
2016-04-16 13:15:41 -0400

    Where-are-thou implementation
    Where-are-thou implementation

---
+++ Where-are-thou.js
@@ -0,0 +1,13 @@
+function whereAreYou(collection, source) {
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function (obj) {
+    // return a Boolean value for `filter` method
+    return srcKeys.every(function (key) {
+      // reduce to Boolean value to be returned for `every` method
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
+
